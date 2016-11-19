61ffb96292ee6b86a5790066af5a013a15d14bc0
alexander <duanhjlt@163.com>
2016-11-19 23:41:28 +0800

    规范化

---
+++ custom/settings.path.sh
@@ -0,0 +1,9 @@
+# PATH
+export PATH=$HOME/bin:/usr/local/bin:$PATH
+export PATH=/opt/local/bin:$PATH
+export PATH=/opt/local/sbin:$PATH
+export PATH=/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources:$PATH
+
+# 设置mac crash分析的环境变量
+export DEVELOPER_DIR="/Applications/Xcode.app/Contents/Developer"
+
