182c37d91491bf8a03526016267d835099cf5bc5
alexander <duanhjlt@163.com>
2017-01-06 10:49:11 +0800

    update

--- custom/settings.alias.sh
+++ custom/settings.alias.sh
@@ -4,6 +4,7 @@ alias la='ls -a'
 alias vi='vim'
 alias javac="javac -J-Dfile.encoding=utf8"
 alias grep="grep --color=auto"
+alias subl="'/Applications/Sublime Text.app/Contents/MacOS/Sublime Text'"
 alias -s html=mate   # 在命令行直接输入后缀为 html 的文件名，会在 TextMate 中打开
 alias -s rb=mate     # 在命令行直接输入 ruby 文件，会在 TextMate 中打开
 #alias -s py=vi       # 在命令行直接输入 python 文件，会用 vim 中打开，以下类似
