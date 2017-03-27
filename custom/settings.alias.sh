2cd71447ebae7ed3dc52841c3fa24b6634183d23
alexander <duanhjlt@163.com>
2017-03-27 15:37:08 +0800

    Add VSCode's short cut

--- custom/settings.alias.sh
+++ custom/settings.alias.sh
@@ -5,6 +5,7 @@ alias vi='vim'
 alias javac="javac -J-Dfile.encoding=utf8"
 alias grep="grep --color=auto"
 alias subl="'/Applications/Sublime Text.app/Contents/MacOS/Sublime Text'"
+alias code="'/Applications/Visual Studio Code.app/Contents/MacOS/Electron'"
 alias -s html=mate   # 在命令行直接输入后缀为 html 的文件名，会在 TextMate 中打开
 alias -s rb=mate     # 在命令行直接输入 ruby 文件，会在 TextMate 中打开
 #alias -s py=vi       # 在命令行直接输入 python 文件，会用 vim 中打开，以下类似
