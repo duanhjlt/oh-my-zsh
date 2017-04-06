719226285493bb42fbe04cf28818c4808ec36b9e
alexander <duanhjlt@163.com>
2017-04-06 23:16:44 +0800

    Add tomact Path

--- custom/settings.path.sh
+++ custom/settings.path.sh
@@ -3,7 +3,7 @@ export PATH=$HOME/bin:/usr/local/bin:$PATH
 export PATH=/opt/local/bin:$PATH
 export PATH=/opt/local/sbin:$PATH
 export PATH=/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources:$PATH
-export PATH=/Users/alexander/Projects/ReposEx/depot_tools:$PATH
+export PATH=~/Library/tomact/bin:$PATH
 
 # 设置mac crash分析的环境变量
 export DEVELOPER_DIR="/Applications/Xcode.app/Contents/Developer"
