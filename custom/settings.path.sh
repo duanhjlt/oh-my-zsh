99a1b26dfe8342958ec30690250cad204ec73ea9
alexander <duanhjlt@163.com>
2017-04-07 22:07:07 +0800

    update

--- custom/settings.path.sh
+++ custom/settings.path.sh
@@ -4,7 +4,6 @@ export PATH=/opt/local/bin:$PATH
 export PATH=/opt/local/sbin:$PATH
 export PATH=/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources:$PATH
 export PATH=~/Library/tomact/bin:$PATH
-export PATH=~/Library/zookeeper/bin:$PATH
 
 # 设置mac crash分析的环境变量
 export DEVELOPER_DIR="/Applications/Xcode.app/Contents/Developer"
