33c58fb86b55b43f01ff75aadbde625b8710fa61
duanhongjin <duanhjlt@163.com>
2017-04-19 12:17:13 +0800

    update

--- custom/settings.path.sh
+++ custom/settings.path.sh
@@ -4,6 +4,7 @@ export PATH=/opt/local/bin:$PATH
 export PATH=/opt/local/sbin:$PATH
 export PATH=/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources:$PATH
 export PATH=~/Library/tomact/bin:$PATH
+export PATH=/usr/libexec:$PATH
 
 # 设置mac crash分析的环境变量
 export DEVELOPER_DIR="/Applications/Xcode.app/Contents/Developer"
