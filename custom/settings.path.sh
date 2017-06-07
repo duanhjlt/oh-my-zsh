05212bda71d79f5439efd77b4a2b8710074ad7c0
duanhongjin <duanhjlt@163.com>
2017-06-07 10:13:58 +0800

    update

--- custom/settings.path.sh
+++ custom/settings.path.sh
@@ -1,15 +1,30 @@
-# PATH
-export PATH=$HOME/bin:/usr/local/bin:$PATH
-export PATH=/opt/local/bin:$PATH
-export PATH=/opt/local/sbin:$PATH
-export PATH=/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources:$PATH
-export PATH=~/Library/tomact/bin:$PATH
-export PATH=/usr/libexec:$PATH
-export PATH=/usr/local/zookeeper-3.4.8/bin:$PATH
-export PATH=/usr/local/redis/bin:$PATH
 
 # 设置mac crash分析的环境变量
 export DEVELOPER_DIR="/Applications/Xcode.app/Contents/Developer"
 
 # JAVA_HOME
-export JAVA_HOME="/Library/Java/JavaVirtualMachines/jdk1.8.0_121.jdk/Contents/Home"
+export JAVA_HOME="/Library/Java/JavaVirtualMachines/jdk1.7.0_71.jdk/Contents/Home"
+
+[ -s "~/.nvm/nvm.sh" ] && . "~/.nvm/nvm.sh" #This loads nvm
+
+[[ -s ~/.autojump/etc/profile.d/autojump.sh ]] && . ~/.autojump/etc/profile.d/autojump.sh
+
+if which swiftenv > /dev/null; then 
+    eval "$(swiftenv init -)"; 
+fi
+
+# gitignore 
+function gi() { 
+    curl -L -s https://www.gitignore.io/api/$@ ;
+}
+
+# PATH
+export PATH=~/bin:/usr/local/bin:$PATH
+export PATH=/opt/local/bin:$PATH
+export PATH=/opt/local/sbin:$PATH
+export PATH=/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources:$PATH
+export PATH=/usr/libexec:$PATH
+export PATH=~/.fastlane/bin:$PATH
+export PATH=~/Projects/Sources/pdfium/depot_tools:$PATH
+
+
