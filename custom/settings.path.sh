d639975f50107942e4016181e3f214248da843b6
duanhongjin <duanhjlt@163.com>
2020-04-07 12:00:27 +0800

    update flutter path

--- custom/settings.path.sh
+++ custom/settings.path.sh
@@ -42,3 +42,4 @@ export PATH=$PATH:~/repo/depot_tools
 export PATH=$PATH:~/Projects/flutter/bin
 export PUB_HOSTED_URL=https://pub.flutter-io.cn
 export FLUTTER_STORAGE_BASE_URL=https://storage.flutter-io.cn
+export FLUTTER_HOME=/Volumes/External/Projects/flutter
