7f1d09eb7af831763f7b2bca6ddbbb1f05277f23
duanhongjin <duanhongjin@360.cn>
2021-05-21 15:59:22 +0800

    update qt bin path

--- custom/settings.path.sh
+++ custom/settings.path.sh
@@ -44,6 +44,7 @@ export PATH=$PATH:~/Library/Android/sdk/platform-tools
 export PATH=$PATH:~/Library/Android/sdk/tools
 export PATH=/usr/local/opt/qt/bin:$PATH
 export PATH=/usr/local/protobuf/bin:$PATH
+export PATH=$PATH:~/Qt/5.15.1/clang_64/bin
 export LDFLAGS="-L/usr/local/opt/qt/lib"
 export CPPFLAGS="-I/usr/local/opt/qt/include"
 export PUB_HOSTED_URL=https://pub.flutter-io.cn
