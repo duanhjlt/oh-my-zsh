cffbf9ebc4443a88bf3a072b50892ea15e134e81
duanhongjin <duanhjlt@gmail.com>
2022-06-11 15:14:11 +0800

    remove unused code

--- custom/settings.alias.sh
+++ custom/settings.alias.sh
@@ -1,14 +1,12 @@
+currentOS=$(uname)
+echo $currentOS
+
 alias cls='clear'
 alias ll='ls -l'
 alias la='ls -a'
 alias vi='vim'
 alias javac="javac -J-Dfile.encoding=utf8"
 alias grep="grep --color=auto"
-alias subl="'/Applications/Sublime Text.app/Contents/MacOS/Sublime Text'"
-alias vscode="'/Applications/Visual Studio Code.app/Contents/MacOS/Electron'"
-alias -s html=mate   # 在命令行直接输入后缀为 html 的文件名，会在 TextMate 中打开
-alias -s rb=mate     # 在命令行直接输入 ruby 文件，会在 TextMate 中打开
-#alias -s py=vi       # 在命令行直接输入 python 文件，会用 vim 中打开，以下类似
 alias -s js=vi
 alias -s c=vi
 alias -s java=vi
@@ -18,11 +16,23 @@ alias -s tgz='tar -xzvf'
 alias -s zip='unzip'
 alias -s bz2='tar -xjvf'
 
+alias g++='g++ -std=c++11'
+
+alias xlog="~/.oh-my-zsh/custom/xlog.sh"
+
+# gitignore 
+function gi() { 
+    curl -L -s https://www.gitignore.io/api/$@ ;
+}
+
+# mac
+if [ "$currentOS" = "Darwin" ] ; then
+
 alias rm=trash
 alias r=trash
 alias rl='ls ~/.trash'
 alias ur=undelfile
-alias g++='g++ -std=c++11'
+
 undelfile()
 {
   mv -i ~/.trash/$@ ./
@@ -32,25 +42,6 @@ trash()
   mv $@ ~/.trash/
 }
 
-alias lg='lazygit'
-
-# Pod
-alias pod-update="pod update --verbose --no-repo-update"
-
-# xdm
-alias xdm="/Applications/XDM.app/Contents/MacOS/XDM do Release "
-alias xdmd="/Applications/XDM.app/Contents/MacOS/XDM do Debug "
-alias xdm2="/Applications/XDM2.app/Contents/MacOS/XDM do \"\" "
-
-# xpm
-alias xpms='/Applications/xpm.app/Contents/MacOS/xpm do simulator '
-alias xpmd='/Applications/xpm.app/Contents/MacOS/xpm do device '
-
-# emacs
-EMACS_HOME=/Applications/Emacs.app/Contents/MacOS
-# 在终端下打开文件,替换命令行默认的emacs
-alias emacs="${EMACS_HOME}/Emacs -nw"
-# GUI方式打开文件
-alias emacsf="${EMACS_HOME}/bin/emacsclient -n"
-
-alias xlog="~/.oh-my-zsh/custom/xlog.sh"
+else
+  
+fi
--- custom/settings.path.sh
+++ custom/settings.path.sh
@@ -1,38 +1,38 @@
+currentOS=$(uname)
+echo $currentOS
+
+# mac
+if [ "$currentOS" = "Darwin" ] ; then
 
 # 设置mac crash分析的环境变量
 export DEVELOPER_DIR="/Applications/Xcode.app/Contents/Developer"
 
 # JAVA_HOME
 export JAVA_HOME="/Library/Java/JavaVirtualMachines/jdk1.8.0_91.jdk/Contents/Home"
-#export JAVA_HOME="/Library/Java/JavaVirtualMachines/jdk-14.0.1.jdk/Contents/Home"
 
-export CLOUDSDK_CORE_PROJECT="richard-prox2"
-
-#export GOPATH=/Users/duanhongjin/go
-#export GOPATH=~/go
 export GOPATH=~/Projects/repos/go
 export GOROOT="/usr/local/go"
 export GOBIN=$GOPATH/bin
 export ANDROID_HOME=~/Library/Android/sdk/
 export ANDROID_NDK_HOME=~/Library/Android/sdk/ndk/ndk-link
 
-[ -s "~/.nvm/nvm.sh" ] && . "~/.nvm/nvm.sh" #This loads nvm
-
-[ -f /usr/local/etc/profile.d/autojump.sh ] && . /usr/local/etc/profile.d/autojump.sh
-
 if which swiftenv > /dev/null; then 
     eval "$(swiftenv init -)"; 
 fi
 
-# gitignore 
-function gi() { 
-    curl -L -s https://www.gitignore.io/api/$@ ;
-}
+else
+# linux
+  
+fi
 
+export PATH=$PATH:~/bin:/usr/local/bin
+
+# mac
+if [ "$currentOS" = "Darwin" ] ; then
+
+export PATH=$PATH:/Library/Apple/usr/bin
 # PATH
 export PATH=$PATH:$GOBIN:$GOROOT/bin
-export PATH=$PATH:/Library/Apple/usr/bin
-export PATH=$PATH:~/bin:/usr/local/bin
 export PATH=$PATH:/opt/local/bin
 export PATH=$PATH:/opt/local/sbin
 export PATH=$PATH:/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources
@@ -56,3 +56,8 @@ export FLUTTER_HOME=~/Projects/flutter
 export GOPROXY=https://goproxy.cn
 export GOSUMDB=sum.golang.org
 export DART_HOME=~/Projects/flutter/bin/cache/dart-sdk/bin
+
+else
+# Linux
+  
+fi
